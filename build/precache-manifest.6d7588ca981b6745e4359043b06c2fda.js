a44893436705caf122275e087027ec90d40a6f26
Gynnaux <gyntlify@gmail.com>
2019-08-25 21:34:39 +0800

    adding files for github pages

---
+++ build/precache-manifest.6d7588ca981b6745e4359043b06c2fda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de45cd7c37ace83707d1",
+    "url": "/lights-out/static/css/main.e200bdb4.chunk.css"
+  },
+  {
+    "revision": "de45cd7c37ace83707d1",
+    "url": "/lights-out/static/js/main.de45cd7c.chunk.js"
+  },
+  {
+    "revision": "03109274422a2c9f0101",
+    "url": "/lights-out/static/js/1.03109274.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "36d7a2a421860342c31e9a5625ed4df7",
+    "url": "/lights-out/index.html"
+  }
+];
